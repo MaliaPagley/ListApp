70f8186c706dd8f6ca6d5238df26d792c0b4ac5c
MaliaPagley <maliapagley@gmail.com>
2023-08-13 10:14:37 -0400

    added starter files
    Added:
    CSS FILE
    JAVASCRIPT FILE
    HTML FILE

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("add-button").addEventListener("click", addCart)
+const input = document.getElementById("input-field")
+
+function addCart() {
+    console.log(`${input.value} `)
+}
