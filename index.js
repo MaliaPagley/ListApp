bbfdd587f65fbf5ce0e197cde4650c26c6ed29b9
MaliaPagley <maliapagley@gmail.com>
2023-08-16 14:19:33 -0400

    added database/js

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const button = document.getElementById("add-button").addEventListener("click", addCart)
-const input = document.getElementById("input-field")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-function addCart() {
-    console.log(`${input.value} `)
+const appSettings = {
+    databaseURL: "https://realtime-database-8d208-default-rtdb.firebaseio.com"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const listInDB = ref(database, "mylist")
+
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const listEl = document.getElementById("my-list")
+
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(listInDB, inputValue)
+    clearInputFieldEl()
+   
+})
+onValue(listInDB, function(snapshot) {
+    let snapshotData =Object.values(snapshot.val())
+    clearListEl()
+    listEl.innerHTML =""
+   
+  for (let i =0; i < snapshotData.length; i++) {
+   appendItemToShoppingListEl(snapshotData[i])
+  }
+
+})
+function clearListEl () {
+    listEl.innerHTML = ""
+}
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+function appendItemToShoppingListEl(itemValue) {
+    listEl.innerHTML += `<li>${itemValue}</li>`
 }
